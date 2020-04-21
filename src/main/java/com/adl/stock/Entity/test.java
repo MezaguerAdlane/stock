22ee6cdfd279b4c1f840c46752dbad0be372855c
MezaguerAdlane <mezagueradlane@gmail.com>
2020-04-21 11:06:37 -0700

    TEST

--- src/main/java/com/adl/stock/Entity/test.java
+++ src/main/java/com/adl/stock/Entity/test.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
     @GetMapping(path = "/Test")
     public String getString() {
-        return "jesuis la ";
+        return "jesuis laKHB ";
     }
 }
